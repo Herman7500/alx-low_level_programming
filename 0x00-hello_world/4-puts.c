011598f4ab084ef7519f5fe635caebd3de5d6747
Herman7500 <acgiri.fombu.pg03452@unn.edu.ng>
2023-02-25 02:51:00 -0800

    correct 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,12 @@
  * File: 4-puts.c 
  * Auth: Achiri herman
  */
-
 #include <stdio.h>
 /**
  * main - entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
