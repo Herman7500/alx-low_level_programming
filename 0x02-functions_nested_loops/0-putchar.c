2a2e2d361ecc8d2279be1b1c49a09948e4b7eeaa
Herman7500 <acgiri.fombu.pg03452@unn.edu.ng>
2023-03-02 09:07:58 -0800

    0-putchar corrected

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - Prints Holberton as a message.
  * _putchar: to print alphabet _putchar
@@ -15,6 +14,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-        _putchar('\n');
+	_putchar('\n');
 	return (0);
 }
