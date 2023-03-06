25f928b11a82306ec08a380b4691c7ce4c13a2a4
Herman7500 <acgiri.fombu.pg03452@unn.edu.ng>
2023-03-06 13:35:18 -0800

    positive,0 negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,9 +8,9 @@
 
 void positive_or_negative(int i)
 {
-	if (i < 0)
-		printf("%d is negative\n ", i);
+	if (i >= 0)
+		printf("%d is zero\n", i);
 	else
-		printf("%d is positive\n ", i);
+		printf("%d is is positive\n ", i );
 }
 
